6f75169f9f6b9932264a8dbdd14b6daae1043f0c
github username <github email address>
2019-04-08 14:15:13 -0600

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,9 +1,23 @@
+require 'pry'
 class StudentsController < ApplicationController
   before_action :set_student, only: :show
-  
+
   def index
     @students = Student.all
   end
+
+
+  def activate
+   if set_student.active == false
+     @student.update(active: true)
+   else
+     @student.update(active: false)
+   end
+   redirect_to student_path(@student)
+ end
+
+
+
 
   def show
   end
